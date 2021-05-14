417b1eae5777686266a3364123ed7eeafd80c04c
vishnu <vishnu.kumarps@claysys.net>
2021-05-14 14:08:23 +0530

    post api

--- src/components/ApiCallExample.vue
+++ src/components/ApiCallExample.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
     <h1>Api Example11</h1>
-    <table border="2">
+    <form @submit="submit()">
+      <input type="text" v-model="postData.name" />
+      <br />
+      <br />
+      <input type="text" v-model="postData.age" />
+      <br />
+      <br />
+      <input type="submit" />
+    </form>
+
+    <h2>Table2</h2>
+    <table border="2" class="table">
       <tr>
         <td>ID</td>
         <td>USER ID</td>
         <td>TITLE</td>
-         <td>BODY</td>
+        <td>BODY</td>
       </tr>
-       <tr v-for="item in dataList" :key="item.id">
-        <td>{{item.id}}</td>
-        <td>{{item.userId}}</td>
-        <td>{{item.title}}</td>
-        <td>{{item.body}}</td>
+      <tr v-for="item in dataList" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.userId }}</td>
+        <td>{{ item.title }}</td>
+        <td>{{ item.body }}</td>
       </tr>
     </table>
   </div>
@@ -23,8 +34,18 @@ import axios from "axios";
 export default {
   //npm install axios vue-axios
   name: "ApiCallExample",
+  methods: {
+    submit(e) {
+      console.log(this.postData);
+      e.preventDefault();
+    },
+  },
   data() {
     return {
+      postData: {
+        name: null,
+        age: null,
+      },
       dataList: [],
     };
   },

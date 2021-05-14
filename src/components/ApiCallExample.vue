32b36b0040c16232a01af3a60891f2807c66537d
vishnu <vishnu.kumarps@claysys.net>
2021-05-14 12:45:04 +0530

    get Api

--- src/components/ApiCallExample.vue
+++ src/components/ApiCallExample.vue
@@ -1,23 +1,39 @@
 <template>
   <div>
-      <h1>Api Example11</h1>
+    <h1>Api Example11</h1>
+    <table border="2">
+      <tr>
+        <td>ID</td>
+        <td>USER ID</td>
+        <td>TITLE</td>
+         <td>BODY</td>
+      </tr>
+       <tr v-for="item in dataList" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.userId}}</td>
+        <td>{{item.title}}</td>
+        <td>{{item.body}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-// import Vue from "vue";
-// import VueAxios from "vue-axios";
-//import axios from "axios";
-//Vue.use(VueAxios.axios);
+import axios from "axios";
 export default {
   //npm install axios vue-axios
   name: "ApiCallExample",
-//   mounted() {
-//     Vue.axios.get('http://dummy.restapiexample.com/api/v1/employees')
-//     .then((resp) => {
-//         console.warn(resp.data);
-//     });
-//   },
+  data() {
+    return {
+      dataList: [],
+    };
+  },
+  mounted() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((resp) => {
+      console.warn(resp);
+      this.dataList = resp.data;
+    });
+  },
 };
 </script>
 

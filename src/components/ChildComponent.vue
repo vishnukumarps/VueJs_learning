71c51e822afe220c8c46ac19c5702ea984e9bbe8
vishnu <vishnu.kumarps@claysys.net>
2021-05-24 15:33:02 +0530

    commit

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,25 +1,31 @@
 <template>
   <div>
     <button v-on:click="updateTitle()">Click Me</button>
-    <h1><slot></slot></h1>
+    {{ name }}
+    <div v-for="skill in skillsArray" :key="skill.name">{{ skill.name }}</div>
+    <br />
+    <br />
+    <div>
+      <slot name="line1"></slot>
+      <slot name="line1"></slot>
+       <slot name="line3"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Child",
+  props: ["name", "skillsArray"],
   methods: {
     updateTitle() {
       this.$emit("changeTitle");
     },
     data() {
-      return {
-        
-      };
+      return {};
     },
   },
 };
 </script>
-
 <style>
 </style>
--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,31 +1,52 @@
 <template>
   <div>
-      <h1>This is a Parent component   {{title}}</h1>
-      <child-component v-on:changeTitle="updateTitle($event)" />
-       <child-component>
-         Slot example
-       </child-component>
+    <h1>This is a Parent component {{ title }}</h1>
+    <child-component name="Vishnu Kumar PS"></child-component>
+    <child-component v-bind:name="title"></child-component>
+    <child-component v-bind:skillsArray="skills"></child-component>
+    <child-component v-on:changeTitle="updateTitle($event)" />
+    <child-component>
+      <h1 slot="line1">Slot example</h1>
+      <h1 slot="line2">Slot example</h1>
+      <img src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png" slot="line3"/>
+    </child-component>
   </div>
 </template>
 <script>
 import ChildComponent from "./ChildComponent.vue";
 export default {
   name: "ParentComponent",
-  components: { 
-      ChildComponent
-       },
+  components: {
+    ChildComponent,
+  },
   methods: {
     updateTitle(title) {
-        this.title=title;
+      this.title = title;
     },
   },
   data() {
     return {
-        title:'Test'
+      title: "Test",
+      skills: [
+        {
+          name: "Frozen Yogurt",
+          required: 1,
+          vMode1: "",
+        },
+        {
+          name: "Ice cream sandwich",
+          required: 3,
+          vMode1: "",
+        },
+        {
+          name: "Eclair",
+          required: 1,
+          vMode1: "",
+        },
+      ],
     };
   },
 };
 </script>
-
 <style>
 </style>
